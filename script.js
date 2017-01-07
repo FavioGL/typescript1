b9af50fcc2ff7ffc2ba66fe6174d0759eace0780
FavioGL <hugofavio@gmail.com>
2017-01-06 21:39:00 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Favio Gonzalez on 1/6/2017.
+ */
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello, " + this.greeting;
+    };
+    return Greeter;
+}());
+var greeter = new Greeter("world");
+var button = document.createElement('button');
+button.textContent = "Say Hello";
+button.onclick = function () {
+    alert(greeter.greet());
+};
+document.body.appendChild(button);
---
+++ script.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by Favio Gonzalez on 1/6/2017.
+ */
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+let greeter = new Greeter("world");
+
+let button = document.createElement('button');
+button.textContent = "Say Hello";
+button.onclick = function() {
+    alert(greeter.greet());
+}
+
+document.body.appendChild(button);
